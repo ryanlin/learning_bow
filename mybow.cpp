d804743b22c4dc4218f8cf64ccbe430c49839cac
ryanlin <ruilinl@nevada.unr.edu>
2019-07-19 14:06:57 -0700

    first commit from laptop

--- mybow.cpp
+++ mybow.cpp
@@ -1,11 +1,20 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
+/*
+*  Compile:
+*  $ g++ mybow.cpp -o mybow `pkg-config --cflags --libs opencv4`
+*
+*  Run:
+*  $ ./mybow
+*/
 
-//#include <opencv2/xfeatures2d.hpp>
+#include <iostream>
+// #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
 int main()
 {
-    cv::Mat myImg = cv::imread("~/Downloads/4loko.jpg", cv::IMREAD_COLOR);
+    cv::Mat myImg = cv::imread("4loko.jpg", cv::IMREAD_COLOR);
 
     cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
     cv::imshow("Image",myImg);
